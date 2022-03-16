bdf0cf5faf7c216e63df289deb4430e14816e210
PDF1994 <101346500+PDF1994@users.noreply.github.com>
2022-03-16 12:07:59 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,2 +1,7 @@
 #script n.1
+#install.packages("raster")
 library(raster)
+#settaggio cartella di lavoro
+> setwd("C:/telerilevamento/lab")
+#assegnazione di un ogetto all'immagine satellitare
+l2011 <- brick("224r63_2011.grd")
